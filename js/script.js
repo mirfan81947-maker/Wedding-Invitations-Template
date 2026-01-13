d7685a72480c371e058aa260273dc2050b45e662
mirfan81947-maker <mirfan81947@gmail.com>
2026-01-13 23:31:53 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,27 +1,26 @@
 "use strict";
 
-var d = new Date(new Date().getTime() + 48 * 120 * 120 * 2000);
-
-    // default example
+    // M Irfan Bhai, yahan maine aapki date set kar di hai
     simplyCountdown('.simply-countdown-one', {
-        year: d.getFullYear(),
-        month: d.getMonth() + 1,
-        day: d.getDate()
+        year: 2035,        // Saal
+        month: 1,          // Mahina (January)
+        day: 5,            // Tareekh
+        enableUtc: false
     });
 
     // inline example
     simplyCountdown('.simply-countdown-inline', {
-        year: d.getFullYear(),
-        month: d.getMonth() + 1,
-        day: d.getDate(),
+        year: 2035,
+        month: 1,
+        day: 5,
         inline: true
     });
 
     //jQuery example
     $('#simply-countdown-losange').simplyCountdown({
-        year: d.getFullYear(),
-        month: d.getMonth() + 1,
-        day: d.getDate(),
+        year: 2035,
+        month: 1,
+        day: 5,
         enableUtc: false
     });
 
